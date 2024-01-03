a67c493909523f4c00524cac0d9f53c966d6f008
Tushar Gupta <tushargupta4870@gmail.com>
2024-01-03 09:13:03 +0530

    made the sidebar for mobile view

--- src/components/Sidebar/HorizontalSidebar.jsx
+++ src/components/Sidebar/HorizontalSidebar.jsx
@@ -1,6 +1,35 @@
-const HorizontalSidebar = () => {
+import { Box, Flex, Tooltip } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
+
+const HorizontalSidebar = ({ sidebarItems }) => {
   return (
-    <div>HorizontalSidebar</div>
-  )
-}
-export default HorizontalSidebar
+    <Box position='sticky' w={'100%'} bg={'gray.100'} >
+      <Flex
+        direction='row'
+        gap={1}
+        pt={2}
+        cursor='pointer'
+        justifyContent={'space-around'}>
+        {sidebarItems.map((item, index) => (
+          <Tooltip
+            key={index}
+            hasArrow
+            label={item.text}
+            placement='bottom'
+            openDelay={500}
+            display={{ base: 'block', md: 'none' }}>
+            <Flex
+              px={3}
+              py={2}
+              _hover={{ borderBottom: '3px solid black' }}
+              as={RouterLink}
+              alignItems={'center'}>
+              <Box >{item.icon}</Box>
+            </Flex>
+          </Tooltip>
+        ))}
+      </Flex>
+    </Box>
+  );
+};
+export default HorizontalSidebar;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,56 @@
 import { Box } from '@chakra-ui/react';
 
 const HomePage = () => {
-  return <Box bg={'gray.100'} minH='92vh'></Box>;
+  return (
+    <Box bg={'gray.100'} minH='92vh'>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam
+      voluptatibus quibusdam tempora quis iure officia est ullam commodi hic
+      molestias, delectus a tenetur. A deserunt quas, dolorem earum adipisci
+      nemo.lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+      exercitationem assumenda tempora dolorem explicabo nam ipsa quo aliquam
+      rerum tenetur minima, minus eius ea obcaecati consequatur, iusto corporis
+      illum asperiores nulla magnam? Iusto suscipit voluptas eligendi voluptatem
+      debitis eveniet, nemo molestias? Maxime natus alias doloribus minima illo
+      iste non adipisci! Dolorem ipsam labore et exercitationem, quasi
+      distinctio saepe quas ullam voluptatibus cumque ad, incidunt, ducimus
+      earum vero. Dolorum delectus consequun Lorem, ipsum dolor sit amet
+      consectetur adipisicing elit. Doloremque, vero nesciunt, reprehenderit
+      ratione labore laboriosam debitis asperiores sequi accusantium reiciendis
+      quasi impedit consequuntur dolores suscipit saepe deleniti officia
+      repudiandae ipsam esse cupiditate. Repellendus saepe totam perspiciatis
+      dolor, cupiditate assumenda tempore necessitatibus praesentium sed
+      excepturi ipsam cum earum facere eligendi odit veniam expedita temporibus
+      explicabo, quo distinctio debitis, reprehenderit est. Magnam corrupti
+      mollitia doloremque quod beatae deleniti nulla maiores. Accusantium, ipsum
+      sapiente? Amet quasi sunt vero asperiores animi optio vel nam facere ab
+      earum tempora, quia cumque libero consectetur cupiditate, quas ipsam.
+      Numquam voluptatum quisquam eius voluptatibus ab doloribus. Quidem esse
+      velit excepturi beatae magnam iure aut harum soluta et laborum?
+      Perspiciatis quibusdam reprehenderit facilis ab optio earum non? Adipisci
+      necessitatibus, eos veniam amet non quasi asperiores natus a et dolore
+      quibusdam laboriosam consectetur voluptas corrupti quos quas totam quia
+      corporis repellendus dolorum! Labore omnis illo maiores temporibus,
+      corporis distinctio vitae, excepturi quis nulla at architecto voluptatem
+      aliquid. Ea asperiores repellendus ratione doloremque reprehenderit vitae
+      expedita earum eum recusandae cupiditate iste quisquam commodi culpa
+      maiores, alias autem, laboriosam consequuntur deserunt nihil nobis
+      blanditiis sint id. Minima sequi, blanditiis natus officiis at doloribus
+      modi obcaecati iste explicabo dolore porro voluptates. Ab, repudiandae.
+      tur incidunt saepe qui enim autem dolorem non, aliquam reprehenderit odio
+      quasi quos illum veritatis dicta odit dignissimos ad minus repellat
+      blanditiis. Dignissimos eaque animi similique excepturi? Consequuntur
+      obcaecati culpa expedita, iusto vero officiis voluptates tenetur
+      recusandae labore mollitia et eaque quisquam eligendi neque ipsa
+      quibusdam! Vero debitis aliquid quae illo itaque. Quod est repudiandae
+      veritatis cumque sunt delectus dolor vel consequatur ullam tempora
+      aspernatur illo earum minus, tenetur maxime. Eum aliquid soluta error
+      officiis placeat voluptatum, aperiam ratione eos sed expedita, repudiandae
+      corporis ullam labore voluptate nihil maxime nemo corrupti minus natus
+      rerum, voluptatibus architecto! Deserunt quasi cumque officia corporis
+      molestias ex similique corrupti, unde repellat sit illum amet ea possimus
+      dolores dolorum, magni pariatur earum dignissimos beatae commodi. Sed
+      expedita quod quia perferendis vel asperiores nesciunt aliquam nobis.
+    </Box>
+  );
 };
 export default HomePage;
